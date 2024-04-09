a3940e3cd46e77c947910067fd06bb7d7676b44f
quands <quan080195@gmail.com>
2024-04-10 00:11:27 +0700

    uodate

--- src/features/user/controller/user.controller.ts
+++ src/features/user/controller/user.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get, Param, Post, Query, Render } from '@nestjs/common';
 import { UserService } from '../service/user.service';
 
-@Controller('')
+@Controller('policy')
 export class UserController {
   constructor(private userService: UserService) { }
 
-  @Get()
+  @Get('vvta')
   @Render('policy')
   getPolicy(){
     // return this.userService.test()
